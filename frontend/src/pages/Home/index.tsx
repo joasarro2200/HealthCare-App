9d42830d9f84dfb5d8a8111e0886382256d6fcbc
Joaquin Sarro <joaquin.sarro2200@gmail.com>
2024-02-20 00:04:06 -0300

    update home page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -5,15 +5,6 @@ const Home: React.FC = () => {
   return (
     <div className='mainContainer'>
         <h1>Welcome!</h1>
-        <p>
-          Our app is designed to simplify the process of registering
-          and managing patient data. Easily create and maintain patient
-          records, ensuring all their information is organized and readily
-          accessible.
-        </p>
-        <p>
-            Simplify your medical facility's administrative tasks with our easy-to-use platform.
-        </p>
     </div>
   );
 };
